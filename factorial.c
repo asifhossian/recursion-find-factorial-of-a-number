f58de1ababffa15d55ccc831b250b5b5ea7d685a
asifhossian <m.asif41.001@gmail.com>
2020-05-05 22:13:33 +0600

    code final

--- factorial.c
+++ factorial.c
@@ -1,16 +1,36 @@
 #include<stdio.h>
-long int multiplyNumbers(int n);
-int main() {
-    int n;
-    printf("Enter a positive integer: ");
-    scanf("%d",&n);
-    printf("Factorial of %d = %ld", n, multiplyNumbers(n));
+
+long long int recusionFactorial(int number)
+{
+    if (number >= 1)
+    {
+        return number * recusionFactorial(number - 1);
+    }
+
+    else
+    {
+        return 1;
+    }
+}
+
+int main()
+{
+    int number;
+
+    printf("Enter an integer number: ");
+
+    scanf("%d",&number);
+
+    if (number < 0)
+    {
+
+        printf("Undefined!!\nFactorial of a negative numbers is undefined.\n");
+
+        return 0;
+    }
+
+    printf("Factorial of %d = %lld", number, recusionFactorial(number));
+
     return 0;
 }
 
-long int multiplyNumbers(int n) {
-    if (n>=1)
-        return n*multiplyNumbers(n-1);
-    else
-        return 1;
-}
